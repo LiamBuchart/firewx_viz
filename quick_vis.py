9a92be0b59da243a2ff70c52d75d6417d44fa5cd
Buchart <liam.buchart@nrcan-rncan.gc.ca>
2025-07-21 08:16:15 -0600

    testing import issue

--- quick_vis.py
+++ quick_vis.py
@@ -9,14 +9,16 @@
 """
 
 import xarray as xr
+import cfgrib
 import matplotlib.pyplot as plt
 
 from pathlib import Path
 
 ##### USER DEFINED VARIABLES #####
 data_dir = "RDPS"
+save_dir = "/FIGURES/"
 # consult the variable list or any data directory (RDPS, HRDPS, etc.) for names/conventions
-vis_var = "RelativeHumidity"
+vis_var = "RelativeHumidity_AGL-2m"
 year = "2025"
 month = "07"
 day = "02"
@@ -24,3 +26,11 @@ model_run = "12"
 fcst_hr = "008"
 ##### END USER DEFINED VARIABLES #####
 
+# define the path to the data
+data_path = str(Path.cwd()) + "/" + data_dir + "/" + year + month + day + "/" + model_run + "/"
+data_file = f"{year}{month}{day}_T{model_run}_MSC_{data_dir}_{vis_var}_RLatLon0.09_PT{fcst_hr}H.grib2"
+print(data_path + "    "  + data_file)
+
+ds = xr.load_dataset(data_path + data_file, engine="cfgrib")
+
+print(ds)
---
+++ test.py
@@ -0,0 +1,7 @@
+print("Im going to take my sweet, sweet time...")
+
+import matplotlib.pyplot as plt
+import xarray as xr
+import cfgrib
+
+print("Is this thing on???")
