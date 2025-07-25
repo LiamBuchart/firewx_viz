21b9b006d4e81c87b92384e08d4f452739102ee8
Buchart <liam.buchart@nrcan-rncan.gc.ca>
2025-07-25 16:28:09 -0600

    now thinking about winds shifts on a grid, weird

---
+++ regrid_data.py
@@ -0,0 +1,62 @@
+"""
+
+    Use xarray and xesmf to regrid onto a unfiorm grid
+    Grid defined locally
+
+    function to be called by other scripts
+
+    Liam.Buchart@nrcan-rncan.gc.ca
+    July 25, 2025
+
+"""
+
+import xarray as xr
+import xesmf as xe
+import numpy as np
+
+# default nice grid to cover canada
+default_lat_range = (41.0, 84.0)  # from 41 to 84 degrees latitude
+default_lon_range = (-141.0, -52.0)  # from -141 to -52 degrees longitude
+default_resolution = 0.1  # resolution in degrees
+
+# define the uniformly spaced grid
+def create_target_grid(lat_range, lon_range, resolution):
+    """
+    Create a target grid with specified latitude and longitude ranges and resolution.
+
+    Parameters:
+    lat_range (tuple): Tuple of (min_lat, max_lat).
+    lon_range (tuple): Tuple of (min_lon, max_lon).
+    resolution (float): Resolution in degrees.
+
+    Returns:
+    xarray.Dataset: The target grid dataset.
+    """
+    lat = np.arange(lat_range[0], lat_range[1], resolution)
+    lon = np.arange(lon_range[0], lon_range[1], resolution)
+    ds_out = xr.Dataset(
+    {
+        "lat": (["lat"], lat, {"units": "degrees_north"}),
+        "lon": (["lon"], lon, {"units": "degrees_east"}),
+    }
+
+    return ds_out
+)
+
+def regrid_data(ds, target_grid):
+    """
+    Regrid the input dataset onto a target grid using xesmf.
+
+    Parameters:
+    ds (xarray.Dataset): The input dataset to be regridded.
+    target_grid (xarray.Dataset): The target grid onto which the data will be regridded.
+
+    Returns:
+    xarray.Dataset: The regridded dataset.
+    """
+    regridder = xe.Regridder(ds, target_grid, 'bilinear')
+    regridded_ds = regridder(ds)
+    return regridded_ds
+
+# make a default grid
+canada_ten_km = create_target_grid(default_lat_range, default_lon_range, default_resolution)
--- wind_shift_map.py
+++ wind_shift_map.py
@@ -10,11 +10,14 @@
 #%%
 import xarray as xr
 import numpy as np
+import cfgrib
 import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
 
 from cartopy import crs as ccrs
 from pathlib import Path
+
+from regrid_data import regrid_data, canada_ten_km
 
 ##### USER DEFINED VARIABLES #####
 data_dir = "RDPS"
@@ -39,11 +42,19 @@ sp = Path(save_dir).resolve()
 
 #%%
 # quick map of the speed and direction data
-def quick_map(ds, var_name, title):
+# from a dataset
+def quick_ds_map(ds, var_name, title):
     plt.figure(figsize=(10, 8))
+    # data resolution
+    resol = '50m'
     ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([-145, -45, 43, 75], crs=ccrs.PlateCarree())
+    # province boundaries
+    provinc_bodr = cfeature.NaturalEarthFeature(category='cultural', 
+                    name='admin_1_states_provinces_lines', scale=resol, facecolor='none', edgecolor='k')
     ax.add_feature(cfeature.COASTLINE)
     ax.add_feature(cfeature.BORDERS, linestyle='-')
+    ax.add_feature(provinc_bodr, linestyle='--', linewidth=0.6, edgecolor="k", zorder=10)
     ax.set_title(title)
     ds[var_name].plot.pcolormesh(ax=ax, 
                                  transform=ccrs.PlateCarree(), 
@@ -52,18 +63,50 @@ def quick_map(ds, var_name, title):
     plt.show()
 
 #%%
+# quick map from a data array
+def quick_da_map(da, var_name, title):
+    plt.figure(figsize=(10, 8))
+    # data resolution
+    resol = '50m'
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([-145, -45, 43, 75], crs=ccrs.PlateCarree())
+    # province boundaries
+    provinc_bodr = cfeature.NaturalEarthFeature(category='cultural', 
+                    name='admin_1_states_provinces_lines', scale=resol, facecolor='none', edgecolor='k')
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS, linestyle='-')
+    ax.add_feature(provinc_bodr, linestyle='--', linewidth=0.6, edgecolor="k", zorder=10)
+    ax.set_title(title)
+    da.plot.pcolormesh(ax=ax, 
+                                 transform=ccrs.PlateCarree(), 
+                                 x="longitude", y="latitude")
+    plt.colorbar(label=var_name)
+    plt.show()
+
+#%%
+# open the datasets using xarray and cfgrib
+# backend_kwargs={'indexpath': ''} is used to avoid creating an index file
 spd = xr.open_dataset(f"{data_dir}{spd_file}", 
                       engine="cfgrib", backend_kwargs={'indexpath': ''})
 dir = xr.open_dataset(f"{data_dir}{dir_file}", 
                       engine="cfgrib", backend_kwargs={'indexpath': ''})
 
-#%%
 
 # %%
-quick_map(spd, wind_comps[0], 
+quick_ds_map(spd, wind_comps[0], 
           f"{wind_comps[0]} at {year}-{month}-{day} {model_run}:{fcst_hr}")
 
 #%%
-quick_map(dir, wind_comps[1], 
+quick_ds_map(dir, wind_comps[1], 
           f"{wind_comps[1]} at {year}-{month}-{day} {model_run}:{fcst_hr}")
+
+#%%
+# get wind speed and direction differences from adjacent grid points
+spd_diff_lon_up = spd[wind_comps[0]].diff('longitude', label='upper')
+spd_diff_lon_lo = spd[wind_comps[0]].diff('longitude', label='lower')
+
+spd_diff_lat_up = spd[wind_comps[0]].diff('latitude', label='upper')
+spd_diff_lat_lo = spd[wind_comps[0]].diff('latitude', label='lower')
+
 # %%
+
