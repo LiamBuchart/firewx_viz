e3877a65f63240bacc4cc5c799f0e0298dd0616e
Buchart <liam.buchart@nrcan-rncan.gc.ca>
2025-09-17 16:01:43 -0600

    reconfigure download and storage

--- windaccum/gen_climatology.py
+++ windaccum/gen_climatology.py
@@ -1,6 +1,10 @@
 """
 
-    Much like gen_means this is just taking a mean of some files
+    Generate 30 year mean, median, std, 25th, 75th, 
+    10th, and 90th percentiles of wind speed, direction, 
+    and wind run for each month and meteorological 
+    season (DJF, MAM, JJA, SON).
+
     Input: nil
     Ouput: 1990-2020 monthly and seasonal mean files (in .h5 files)
            of wind speed, direction, and run.
@@ -9,4 +13,18 @@
     September 12, 2025
 
 """
+#%%
+import os
+import json
+
 import xarray as xr
+
+data_dir = "./climatology/"
+
+months = range(1, 12+1)
+seasons = {
+    "DJF": [12, 1, 2],
+    "MAM": [3, 4, 5],
+    "JJA": [6, 7, 8],
+    "SON": [9, 10, 11]
+}
--- windaccum/utils.py
+++ windaccum/utils.py
@@ -6,8 +6,8 @@
 from datetime import date, timedelta
 
 def get_dates_in_year(year):
-    start = date(year-1, 12, 1)
-    end = date(year, 11, 30)
+    start = date(year, 1, 1)
+    end = date(year, 12, 31)
     delta = timedelta(days=1)
     dates = []
     current = start
@@ -15,3 +15,15 @@ def get_dates_in_year(year):
         dates.append(current.strftime('%Y%m%d'))
         current += delta
     return dates
+
+def get_date_from_years(yearS, yearE, month, day):
+    dates = []
+    for year in range(yearS, yearE + 1):
+        try:
+            date_obj = date(year, month, day)
+            dates.append(date_obj.strftime('%Y%m%d'))
+        except ValueError:
+            continue
+    return dates
+
+
